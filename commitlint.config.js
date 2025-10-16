5dd801c385e233e663bcba25c7d9fbe105418573
qinzhenya <qinzhenya@tuzhai.com>
2025-10-16 15:50:54 +0800

    chore: add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'build',
+        'ci',
+        'revert',
+      ],
+    ],
+    'subject-max-length': [2, 'always', 50],
+  },
+};
